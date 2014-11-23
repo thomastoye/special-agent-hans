8e93a83bd557f9b31f67072b60595300f7659f35
Thomas Toye <toye.thomas@gmail.com>
2014-11-23 14:53:51 +0100

    Post score to db

---
+++ backend/api/highscore.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once("database.php");
+
+/**
+  * Post a highscore to the database
+  */
+
+function serverError(){
+	header('HTTP/1.1 500 Internal Server Error');
+    exit();
+}
+
+$mysqli = new mysqli($db["host"], $db["user"], $db["password"], $db["database"]);
+
+if ($mysqli->connect_errno) {
+    serverError();
+}
+
+
+if (!($stmt = $mysqli->prepare("INSERT INTO wa_opdr2_highscores(name, score) VALUES (?,?)"))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+
+$name = $_POST["name"];
+$score = $_POST["score"];
+
+echo "name " . $name . " score " . $score . "\n\n";
+
+if (!$stmt->bind_param("si", $name, $score)) {
+    echo "Binding parameters failed {name}: (" . $stmt->errno . ") " . $stmt->error;
+    serverError();
+}
+
+if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    serverError();
+}
